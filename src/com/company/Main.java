b97553ee84134e8dc736c719252c09bffdc4f5e5
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-06 11:51:59 +0700

    'veHinh'

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //bài 1
+//        for (int i = 1 ; i <= 5 ; i ++ ){
+//            for (int j = 1 ; j <= i ; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+        //bài 2
+//        for (int i = 1 ; i <= 5 ; i ++ ){
+//            for (int j = 5 ; j >= i ; j--){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+        //bài 3
+//        for (int i = 1 ; i <= 3 ; i ++ ){
+//            for (int j = 1 ; j <= 7 ; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+        //bài 4
+        //cách 1
+//        for (int i = 1 ; i <= 6 ; i ++ ){
+//            for (int j = 6 ; j>=i ; j-- ){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1 ; k <= i ; k++){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+        //cách 2
+//        for (int i = 0 ; i <= 10 ; i++){
+//            if (i%2==0){
+//                continue;
+//            }
+//            for (int j = 10 ; j >i/2;j--){
+//                System.out.print(" ");
+//            }
+//            for (int k = 0 ; k<i;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+    }
+}
